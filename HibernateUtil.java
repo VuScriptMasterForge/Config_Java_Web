c30377f367ba0878eba66ed7deb63e0663933784
Nong Hoang Vu <117331143+VuScriptMasterForge@users.noreply.github.com>
2024-04-14 19:25:10 +0700

    Update and rename JPAUtil.java to HibernateUtil.java

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class JPAUtil {
+public class HibernateUtil {
     private static EntityManagerFactory factory;
     public EntityManager em = getEntityManager();
 
